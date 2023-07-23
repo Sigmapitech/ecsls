5cdba803e7ce574687df6ea52e27759bb4af3688
Sigmanificient <edhyjox@gmail.com>
2023-07-23 20:03:28 +0200

    Add vera++ to flake packages

--- flake.nix
+++ flake.nix
@@ -1,14 +1,17 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils } @inputs:
+  outputs = { self, nixpkgs, utils } :
     utils.lib.eachDefaultSystem (system:
       with import nixpkgs { inherit system; }; {
         devShells.default = mkShell {
-          packages = [ python311 ];
+          packages = [
+            python311
+            banana-vera
+          ];
         };
       });
 }
