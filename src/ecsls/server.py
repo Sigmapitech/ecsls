39b41b491a7f4c45a750352b4c0bc66695bf1670
Clément <clement2104.boillot@gmail.com>
2023-07-24 00:15:55 +0200

    Add working lsp

--- src/ecsls/server.py
+++ src/ecsls/server.py
@@ -1,53 +1,49 @@
 from .version import __version__
+from .vera import ReportType, Report, get_vera_output
 
 from pygls.server import LanguageServer
 from lsprotocol.types import (
-    TEXT_DOCUMENT_COMPLETION,
+    TEXT_DOCUMENT_DID_CHANGE,
     TEXT_DOCUMENT_DID_OPEN,
-    CompletionItem,
-    CompletionList,
-    CompletionOptions,
-    CompletionParams,
     Diagnostic,
     DidOpenTextDocumentParams,
     Position,
     Range,
 )
 
+
 server = LanguageServer('ecsls', __version__)
 
-@server.feature(
-    TEXT_DOCUMENT_COMPLETION,
-    CompletionOptions(trigger_characters=["!"])
-)
-def completions(_params: CompletionParams):
-    return CompletionList(
-        is_incomplete=False,
-        items=[
-            CompletionItem(label="its"),
-            CompletionItem(label="working"),
-        ]
-    )
+class LineRange(Range):
+    def __init__(self, line: int):
+        super().__init__(
+            start=Position(line - 1, 1),
+            end=Position(line - 1, 80)
+        )
+
+def get_diagnolstics(filename: str):
+    reports = get_vera_output(filename)
+    return [
+        Diagnostic(
+            range=LineRange(report.line),
+            message=report.rule,
+            source="Json Server",
+        )
+        for report in reports
+    ]
+
 
 @server.feature(TEXT_DOCUMENT_DID_OPEN)
-async def did_open(ls, params: DidOpenTextDocumentParams):
+async def did_open(ls: LanguageServer, params: DidOpenTextDocumentParams):
     """Text document did open notification."""
-    ls.show_message("Text Document Did Open")
-
-    # Get document from workspace
     text_doc = ls.workspace.get_document(params.text_document.uri)
-    line = 1
-    col = 1
+    name = text_doc.uri.partition('://')[2]
+    ls.publish_diagnostics(text_doc.uri, get_diagnolstics(name))
 
-    diagnostic = Diagnostic(
-        range=Range(
-            start=Position(line - 1, col - 1),
-            end=Position(line - 1, col)
-        ),
-        message="Custom validation message",
-        source="Json Server"
-    )
 
-    # Send diagnostics
-    ls.publish_diagnostics(text_doc.uri, [diagnostic])
+@server.feature(TEXT_DOCUMENT_DID_CHANGE)
+async def did_changement(ls: LanguageServer, params: DidOpenTextDocumentParams):
+    text_doc = ls.workspace.get_document(params.text_document.uri)
+    name = text_doc.uri.partition('://')[2]
+    ls.publish_diagnostics(text_doc.uri, get_diagnolstics(name))
 
