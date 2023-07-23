ed230e792539bf4d0d88d43adf3f6c85f499a5c5
Sigmanificient <edhyjox@gmail.com>
2023-07-23 23:43:14 +0200

    Add vera output parser

---
+++ src/ecsls/vera.py
@@ -0,0 +1,75 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import StrEnum
+
+import re
+import subprocess
+from typing import Final, List, Optional
+
+
+REPORT_FORMAT: Final[re.Pattern] = re.compile(
+    r"^[^:]+:(?P<line>\d+):\s?(?P<type>MAJOR|MINOR|INFO):(?P<rule>C-\w\d)$"
+)
+
+class ReportType(StrEnum):
+    MAJOR = "MAJOR"
+    MINOR = "MINOR"
+    INFO = "INFO"
+
+
+@dataclass
+class Report:
+    line: int
+    type: ReportType
+    rule: str
+
+    @classmethod
+    def from_string(cls, line: str) -> Optional[Report]:
+        match = re.match(REPORT_FORMAT, line)
+
+        if match is None:
+            return None
+
+        line, typ, rule = match.groups()
+
+        return cls(
+            line=int(line),
+            type=ReportType(typ),
+            rule=rule
+        )
+
+
+def parse_vera_output(raw_report: str) -> List[Report]:
+    reports = raw_report.split('\n')
+
+    out = []
+    for reported_line in reports:
+        report = Report.from_string(reported_line)
+
+        if report is None:
+            continue
+
+        out.append(report)
+
+    return out
+
+
+def get_vera_output(filename: str):
+    out = subprocess.run(
+        (
+            "vera++",
+            "--profile",
+            "epitech",
+            "--root",
+            "banana-coding-style-checker/vera",
+            filename,
+        ),
+        capture_output=True
+    ).stdout
+
+    return parse_vera_output(out.decode())
+
+
+if __name__ == '__main__':
+    print(get_vera_output("caca.c"))
