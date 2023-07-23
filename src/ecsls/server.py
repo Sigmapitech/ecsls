f25b5ac67c16881805790e0d9cb483bac388a298
Clément <clement2104.boillot@gmail.com>
2023-07-24 01:32:03 +0200

    Add severity

--- src/ecsls/server.py
+++ src/ecsls/server.py
@@ -1,11 +1,12 @@
 from .version import __version__
-from .vera import get_vera_output
+from .vera import get_vera_output, ReportType
 
 from pygls.server import LanguageServer
 from lsprotocol.types import (
     TEXT_DOCUMENT_DID_CHANGE,
     TEXT_DOCUMENT_DID_OPEN,
     Diagnostic,
+    DiagnosticSeverity,
     DidOpenTextDocumentParams,
     Position,
     Range,
@@ -18,6 +19,11 @@ class LineRange(Range):
     def __init__(self, line: int):
         super().__init__(start=Position(line - 1, 1), end=Position(line - 1, 80))
 
+SEVERITIES = {
+    ReportType.MAJOR: 1,
+    ReportType.MINOR: 2,
+    ReportType.INFO: 3,
+}
 
 def get_diagnolstics(filename: str):
     reports = get_vera_output(filename)
@@ -26,6 +32,7 @@ def get_diagnolstics(filename: str):
             range=LineRange(report.line),
             message=report.message,
             source="Json Server",
+            severity=DiagnosticSeverity(SEVERITIES[report.type] + 1),
         )
         for report in reports
     ]
@@ -44,3 +51,4 @@ async def did_changement(ls: LanguageServer, params: DidOpenTextDocumentParams):
     text_doc = ls.workspace.get_document(params.text_document.uri)
     name = text_doc.uri.partition("://")[2]
     ls.publish_diagnostics(text_doc.uri, get_diagnolstics(name))
+
