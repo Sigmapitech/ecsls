5a861324b0bfa2cbdab0fdf48116525062460ce3
Sigmanificient <edhyjox@gmail.com>
2023-07-23 22:12:16 +0200

    Add diagnostic example

--- src/ecsls/server.py
+++ src/ecsls/server.py
@@ -3,10 +3,15 @@ from .version import __version__
 from pygls.server import LanguageServer
 from lsprotocol.types import (
     TEXT_DOCUMENT_COMPLETION,
+    TEXT_DOCUMENT_DID_OPEN,
     CompletionItem,
     CompletionList,
     CompletionOptions,
     CompletionParams,
+    Diagnostic,
+    DidOpenTextDocumentParams,
+    Position,
+    Range,
 )
 
 server = LanguageServer('ecsls', __version__)
@@ -23,4 +28,26 @@ def completions(_params: CompletionParams):
             CompletionItem(label="working"),
         ]
     )
+
+@server.feature(TEXT_DOCUMENT_DID_OPEN)
+async def did_open(ls, params: DidOpenTextDocumentParams):
+    """Text document did open notification."""
+    ls.show_message("Text Document Did Open")
+
+    # Get document from workspace
+    text_doc = ls.workspace.get_document(params.text_document.uri)
+    line = 1
+    col = 1
+
+    diagnostic = Diagnostic(
+        range=Range(
+            start=Position(line - 1, col - 1),
+            end=Position(line - 1, col)
+        ),
+        message="Custom validation message",
+        source="Json Server"
+    )
+
+    # Send diagnostics
+    ls.publish_diagnostics(text_doc.uri, [diagnostic])
 
