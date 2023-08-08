45f93c71fc25f5ac0249700bb1801da8f574dc02
Sigmanificient <edhyjox@gmail.com>
2023-08-08 18:37:55 +0200

    Add nix derivation

--- flake.nix
+++ flake.nix
@@ -1,18 +1,36 @@
 {
+  description = "Epitech Coding Style Checker Language Server";
+
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils } :
-    utils.lib.eachDefaultSystem (system:
-      with import nixpkgs { inherit system; }; {
-        devShells.default = mkShell {
-          packages = [
-            python311
-            banana-vera
-            black
-          ];
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          python311
+          banana-vera
+          black
+        ];
+      };
+
+      packages = {
+        ecsls = let
+          pypkgs = pkgs.python311Packages;
+        in pypkgs.buildPythonPackage {
+          pname = "sourcery-analytics";
+          version = "0.0.1";
+          src = ./.;
+
+          propagatedBuildInputs = [ pypkgs.pygls ];
         };
-      });
+
+        default = self.packages.${system}.ecsls;
+      };
+    });
 }
