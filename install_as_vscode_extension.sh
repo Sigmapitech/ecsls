c16ddbc6ed01815972280d8d04f51d35c5da9cc2
Ciznia <gabriel.hosquet@epitech.eu>
2025-01-08 15:40:57 +0100

    fix packages unpacking and cmake missing dependancies

--- install_as_vscode_extension.sh
+++ install_as_vscode_extension.sh
@@ -25,7 +25,7 @@ echo "Checking dependencies..."
     echo "All dependencies are installed."
 }
 
-install_dependancies() {
+install_dependencies() {
     # Check if /etc/os-release exists
     if [ ! -f /etc/os-release ]; then
         die "Error: /etc/os-release file not found."
@@ -38,24 +38,24 @@ install_dependancies() {
     case "$ID" in
         ubuntu|debian)
             echo "Detected: Debian-based distribution"
-            packages="cmake tcl-dev libboost-dev python3 python-is-python3"
-            echo "Updating package lists and installing packages: $packages"
+            packages=("cmake" "tcl-dev" "python3" "python-is-python3" "libboost-all-dev")
+            echo "Updating package lists and installing packages: ${packages[@]}"
             sudo apt-get update -y
-            sudo apt-get install -y "$packages"
+            sudo apt-get install -y "${packages[@]}" || die "Failed to install packages"
             ;;
         fedora|rhel|centos|rocky|almalinux)
             echo "Detected: Red Hat-based distribution"
-            packages="tcl cmake boost-devel python3"
-            echo "Updating package lists and installing packages: $packages"
+            packages=("tcl-devel" "cmake" "boost-devel" "python3" "boost-devel" "python3.10")
+            echo "Updating package lists and installing packages: ${packages[@]}"
             sudo dnf update -y
-            sudo dnf install -y "$packages"
+            sudo dnf install -y "${packages[@]}" || die "Failed to install packages"
             ;;
         arch|manjaro)
             echo "Detected: Arch-based distribution"
-            packages="cmake tcl boost python3"
-            echo "Updating package lists and installing packages: $packages"
+            packages=("cmake" "tcl" "python3" "boost-libs")
+            echo "Updating package lists and installing packages: ${packages[@]}"
             sudo pacman -Syu --noconfirm
-            sudo pacman -S --noconfirm "$packages"
+            sudo pacman -S --noconfirm "${packages[@]}" || die "Failed to install packages"
             ;;
         *)
             die "Error: Unsupported Linux distribution ($ID)"
@@ -70,10 +70,15 @@ install_vera()  {
     clone_helper "Epitech/banana-vera"  ".vera"
     cd .vera || die "cd failed"
 
-    install_dependancies
-    cmake . -DVERA_LUA=OFF -DPANDOC=OFF -DVERA_USE_SYSTEM_BOOST=ON
+    install_dependencies || die "dependencies installation failed"
+    cmake . -DVERA_LUA=OFF -DPANDOC=OFF -DVERA_USE_SYSTEM_BOOST=ON || die "cmake failed"
     make -j
-    sudo make install
+    sudo make install || die "make failed"
+
+    # Install Python dependencies globally
+    echo "Installing global Python dependencies for Vera..."
+    python3 -m pip install --user clang libclang || die "Failed to install clang library globally"
+
 
     cd ..
     rm -fr .vera
@@ -81,30 +86,43 @@ install_vera()  {
 
 install_rules()
 {
-    clone "Epitech/banana-coding-style-checker" "$HOME/.config/ecsls"
+    clone_helper "Epitech/banana-coding-style-checker" "$HOME/.config/ecsls"
+}
+
+install_lsp() {
+    CLONE_DIR=".$1"
+
+    echo "Cloning repository..."
+    clone_helper "Sigmapitech/$1" $CLONE_DIR
+    cd "$CLONE_DIR" || die "Failed to change directory"
+    (python -m venv venv && venv/bin/pip install -e .) || die "Failed to install ecsls"
+    local TARGET_PATH="$PWD/venv/bin/${1}_run"
+    sudo ln -sf "$TARGET_PATH" "/usr/local/bin/${1}_run" || die "Failed to create symlink"
+    cd ..
 }
 
 install_ecsls() {
-    CLONE_DIR=".ecsls"
+    install_lsp "ecsls"
+}
 
-    echo "Cloning repository..."
-    clone_helper "Sigmapitech/ecsls" $CLONE_DIR
-    cd "$CLONE_DIR" || die "Failed to change directory"
-    (python -m venv venv && venv/bin/pip install -e .) || die "Failed to install ecsls"
-    sudo ln -sf "$PWD"/venv/bin/ecsls_run /usr/local/bin/ecsls_run || die "Failed to create symlink"
+install_lambdadana() {
+    curl -L https://github.com/Epitech/lambdananas/releases/download/v2.4.3.2/lambdananas > lambdananas
+    chmod +x lambdananas
+    sudo mv ./lambdananas /usr/local/bin/
+}
+
+install_ehcsls() {
+    install_lsp "ehcsls"
 }
 
 install_extension() {
-    # Clone the repository
-    CLONE_DIR="epitech-cs"
-
     echo "Cloning repository..."
-    clone_helper "Ciznia/epitech-cs" $CLONE_DIR
-    cd "$CLONE_DIR" || die "Failed to change directory"
+    clone_helper "Ciznia/$1" $1
+    cd "$1" || die "Failed to change directory"
 
     # Package the extension
     echo "Packaging the extension..."
-    npm i
+    npm i || die "failed to install npm dependencies"
     VSIX_FILE=$(npx vsce package | grep -oP 'Packaged: \K.*\.vsix')
 
     # Check if packaging succeeded and install the extension
@@ -120,7 +138,7 @@ install_extension() {
     # Return to the original directory and clean up
     cd ..
     echo "Cleaning up..."
-    rm -rf "$CLONE_DIR"
+    rm -rf "$1"
 }
 
 # Run the dependency check
@@ -128,13 +146,20 @@ check_dependencies
 if ! command -v "vera++" &>/dev/null; then
     install_vera
 fi
-if [ -d ~/.config/ecsls ]; then
+if ! [ -d ~/.config/ecsls ]; then
     install_rules
 fi
 if ! command -v "ecsls_run" &>/dev/null; then
     install_ecsls
 fi
+if ! command -v "lambdananas" &>/dev/null; then
+    install_lambdadana
+fi
+if ! command -v "ehcsls_run" &>/dev/null; then
+    install_ehcsls
+fi
 
-install_extension
+install_extension "epitech-cs"
+install_extension "epitech-hcs"
 
 echo "Done!"
